8438510420ca480914bff1727c12360fa9ab7176
ammathnad <33483733+ammathnad@users.noreply.github.com>
2021-07-24 14:22:55 +0530

    Update exe_shell.sh

--- exe_shell.sh
+++ exe_shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "`date`-Aditya">/tmp/execution_output
 echo "`date`-Aditya">/tmp/execution_output
-echo "`date`-Aditya">/tmp/execution_output
+echo "`date`-Aditya1">/tmp/execution_output
