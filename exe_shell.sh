60cb4cbae154d77d887c699aa9a2c90d9bdc619b
ammathnad <33483733+ammathnad@users.noreply.github.com>
2021-07-25 16:07:44 +0530

    Update exe_shell.sh

--- exe_shell.sh
+++ exe_shell.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo "`date`-Aditya">/tmp/execution_output
-echo "`date`-Aditya">/tmp/execution_output
-echo "`date`-Aditya1">/tmp/execution_output
-echo "`date`-Aditya2">/tmp/execution_output
+git pull https://github.com/ammathnad/Testing.git
