2622121022864cbff3224808a382505868231492
Aditya <aditya@dikshatech.com>
2021-07-28 21:03:22 +0530

    Adding Ansible

--- exe_shell.sh
+++ exe_shell.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd /var/lib/jenkins/workspace/TestingGIT/Testing/ 
 git pull https://github.com/ammathnad/Testing.git
+cd /var/lib/jenkins/workspace/TestingGIT/Testing/fm_test_pol/
+make clean;make
+chown -R jenkins:jenkins /var/lib/jenkins/workspace/TestingGIT/Testing/fm_test_pol/
+cd /var/lib/jenkins/workspace/TestAnsiblePlugin
+ansible-playbook helloworld.yml -i hosts -f 5
